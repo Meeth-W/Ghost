63f683ddec9d4f70970e7f152c9cc6c04a1d92e1
Meeth <meeth2111@gmail.com>
2024-10-07 03:22:05 +0530

    🔥 Initial Setup

---
+++ utils/data.js
@@ -0,0 +1,18 @@
+import PogObject from "../../PogData";
+
+export const data = new PogObject("Ghost", {
+    recently_closed: false,
+    slotBinding: {
+        presets: [
+            {}, {}, {}, {}, {}, {}
+        ],
+        history: [
+            { '36':null, '37':null, '38':null, '39':null, '40':null, '41':null, '42':null, '43':null, '44':null },
+            { '36':null, '37':null, '38':null, '39':null, '40':null, '41':null, '42':null, '43':null, '44':null },
+            { '36':null, '37':null, '38':null, '39':null, '40':null, '41':null, '42':null, '43':null, '44':null },
+            { '36':null, '37':null, '38':null, '39':null, '40':null, '41':null, '42':null, '43':null, '44':null },
+            { '36':null, '37':null, '38':null, '39':null, '40':null, '41':null, '42':null, '43':null, '44':null },
+            { '36':null, '37':null, '38':null, '39':null, '40':null, '41':null, '42':null, '43':null, '44':null },
+        ]
+    }
+}, 'data/mainData.json')

417300b55ad229eaa3e7b3e1a337311c721314af
Meeth <meeth2111@gmail.com>
2024-10-07 04:16:44 +0530

    ✅ Blood Splits

---
+++ modules/bloodSplits/index.js
@@ -0,0 +1,79 @@
+import config from '../../config';
+import { chat } from '../../utils/utils';
+
+const S32PacketConfirmTransaction = Java.type("net.minecraft.network.play.server.S32PacketConfirmTransaction");
+
+const bloodStartMessages = [
+    "[BOSS] The Watcher: Things feel a little more roomy now, eh?",
+    "[BOSS] The Watcher: Oh.. hello?",
+    "[BOSS] The Watcher: I'm starting to get tired of seeing you around here...",
+    "[BOSS] The Watcher: You've managed to scratch and claw your way here, eh?",
+    "[BOSS] The Watcher: So you made it this far... interesting.",
+    "[BOSS] The Watcher: Ah, we meet again...",
+    "[BOSS] The Watcher: Ah, you've finally arrived.",
+]
+
+let bloodStartTime = Date.now()
+let display = false
+let displayText = 'Invalid'
+let bloodStartTicks
+
+const trigger = register("chat", (message) => {
+    if (!bloodStartMessages.includes(message)) return
+    bloodStartTime = Date.now()
+    bloodStartTicks = 0
+    tickCounter.register();
+}).setCriteria("${message}").unregister();
+
+const tickCounter = register('packetReceived', () => {
+    bloodStartTicks++
+    if (bloodStartTicks >= 40) tickCounter.unregister();
+}).setFilteredClass(S32PacketConfirmTransaction).unregister();
+
+const mainTrigger = register("chat", () => {
+    let bloodMove = ((Math.floor((Date.now() - bloodStartTime) / 10) / 100) + 0.10).toFixed(2)
+    let bloodMoveTicks = (bloodStartTicks * 0.05 + 0.1).toFixed(2)
+    let bloodMovePrediction
+    let bloodMovePredictionTicks
+
+    chat(`&cTime&b: &6${bloodMove} Seconds`)
+    chat(`&cTicks&b: &6${bloodMoveTicks} &8(&7${((bloodMoveTicks) / 0.05).toFixed(0)} Ticks&8)`)
+
+    if (bloodMoveTicks >= 31 && bloodMoveTicks <= 33.99) bloodMovePredictionTicks = 36
+    if (bloodMove >= 31 && bloodMove <= 33.99) bloodMovePrediction = 36
+    if (bloodMoveTicks >= 28 && bloodMoveTicks <= 30.99) bloodMovePredictionTicks = 33
+    if (bloodMove >= 28 && bloodMove <= 30.99) bloodMovePrediction = 33
+    if (bloodMoveTicks >= 25 && bloodMoveTicks <= 27.99) bloodMovePredictionTicks = 30
+    if (bloodMove >= 25 && bloodMove <= 27.99) bloodMovePrediction = 30
+    if (bloodMoveTicks >= 22 && bloodMoveTicks <= 24.99) bloodMovePredictionTicks = 27
+    if (bloodMove >= 22 && bloodMove <= 24.99) bloodMovePrediction = 27
+    if (bloodMoveTicks >= 1 && bloodMoveTicks <= 21.99) bloodMovePredictionTicks = 24
+    if (bloodMove >= 1 && bloodMove <= 21.99) bloodMovePrediction = 24
+    if (!bloodMovePrediction) bloodMovePrediction = "Invalid"
+    if (!bloodMovePredictionTicks) bloodMovePredictionTicks = "Invalid"
+
+    chat(`&cMove Prediction&b: &6${bloodMovePredictionTicks} Seconds`)
+    displayText = `&6${bloodMovePredictionTicks}`
+    display = true
+
+    setTimeout(() => { display = false }, 2000)
+}).setCriteria("[BOSS] The Watcher: Let's see how you can handle this.").unregister();
+
+const renderTrigger = register("renderOverlay", () => {
+    if (!display) return
+    Client.Companion.showTitle(`&cBlood Camp Ready!`, `&7Watcher will move at ${displayText}`, 0, 2, 0)
+}).unregister();
+
+export function toggle() {
+    if (config().bloodHelperToggle && config().toggle && config().watcherMoveDisplay) {
+        trigger.register()
+        mainTrigger.register()
+        renderTrigger.register()
+        return
+    }
+    trigger.unregister()
+    mainTrigger.register()
+    renderTrigger.unregister()
+    return
+}
+export default { toggle };

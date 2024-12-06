e148c4485c12b635dd9c7479405733732dc9a10d
Meeth <meeth2111@gmail.com>
2024-12-06 16:23:16 +0530

    ✨ Added Splits

--- modules/bloodSplits/index.js
+++ modules/bloodSplits/index.js
@@ -27,38 +27,38 @@ const trigger = register("chat", (message) => {
 
 const tickCounter = register('packetReceived', () => {
     bloodStartTicks++
-    if (bloodStartTicks >= 40*20) tickCounter.unregister();
 }).setFilteredClass(S32PacketConfirmTransaction).unregister();
+
+let bloodMovePredictionTicks
 
 const mainTrigger = register("chat", () => {
     let bloodMove = ((Math.floor((Date.now() - bloodStartTime) / 10) / 100) + 0.10).toFixed(2)
     let bloodMoveTicks = (bloodStartTicks * 0.05 + 0.1).toFixed(2)
-    let bloodMovePrediction
-    let bloodMovePredictionTicks
 
     let bloodLag = (bloodMove - bloodMoveTicks)
     chat(`&cTime&b: &6${bloodMove} Seconds`)
     chat(`&cTicks&b: &6${bloodMoveTicks} &8(&7${((bloodMoveTicks) / 0.05).toFixed(0)} Ticks&8)`)
 
     if (bloodMoveTicks >= 31 && bloodMoveTicks <= 33.99) bloodMovePredictionTicks = (36 + bloodLag).toFixed(2)
-    if (bloodMove >= 31 && bloodMove <= 33.99) bloodMovePrediction = 36
     if (bloodMoveTicks >= 28 && bloodMoveTicks <= 30.99) bloodMovePredictionTicks = (33 + bloodLag).toFixed(2)
-    if (bloodMove >= 28 && bloodMove <= 30.99) bloodMovePrediction = 33
     if (bloodMoveTicks >= 25 && bloodMoveTicks <= 27.99) bloodMovePredictionTicks = (30 + bloodLag).toFixed(2)
-    if (bloodMove >= 25 && bloodMove <= 27.99) bloodMovePrediction = 30
     if (bloodMoveTicks >= 22 && bloodMoveTicks <= 24.99) bloodMovePredictionTicks = (27 + bloodLag).toFixed(2)
-    if (bloodMove >= 22 && bloodMove <= 24.99) bloodMovePrediction = 27
     if (bloodMoveTicks >= 1 && bloodMoveTicks <= 21.99) bloodMovePredictionTicks = 24
-    if (bloodMove >= 1 && bloodMove <= 21.99) bloodMovePrediction = 24
-    if (!bloodMovePrediction) bloodMovePrediction = "Invalid"
     if (!bloodMovePredictionTicks) bloodMovePredictionTicks = "Invalid"
 
-    chat(`&cMove Prediction&b: &6${bloodMovePredictionTicks} Seconds`)
+    chat(`&cMove Prediction&b: ${(bloodMovePredictionTicks>29.99)? "&c": (bloodMovePredictionTicks>25.66)? '&e': '&a'}&l${bloodMovePredictionTicks} Seconds&r&c!`)
     displayText = `&6${bloodMovePredictionTicks}`
     display = true
-
-    setTimeout(() => { display = false }, 2000)
+    setTimeout(() => { display = false }, 1250)
 }).setCriteria("[BOSS] The Watcher: Let's see how you can handle this.").unregister();
+
+const finalTrigger = register('chat', () => {
+    tickCounter.unregister();
+    if (bloodMovePredictionTicks != 'Invalid')
+    chat(`&cMoved: ${(bloodMovePredictionTicks>29.99)? "&c": (bloodMovePredictionTicks>25.66)? '&e': '&a'}${bloodMovePredictionTicks}s.`)
+    chat(`&cCleared: ${(((bloodStartTicks/20 )- bloodMovePredictionTicks) > 40)? "&c": (((bloodStartTicks/20 )- bloodMovePredictionTicks) > 37)? '&e': '&a'}${((bloodStartTicks/20 )- bloodMovePredictionTicks).toFixed(2)}s.`)
+    chat(`&cTotal: ${((bloodStartTicks/20) > 69)? "&c": ((bloodStartTicks/20) > 62)? '&e': '&a'}${(bloodStartTicks/20).toFixed(2)}s.s`)
+}).setCriteria(`[BOSS] The Watcher: You have proven yourself. You may pass.`).unregister();
 
 const renderTrigger = register("renderOverlay", () => {
     if (!display) return
@@ -70,11 +70,13 @@ export function toggle() {
         trigger.register()
         mainTrigger.register()
         renderTrigger.register()
+        finalTrigger.register()
         return
     }
     trigger.unregister()
     mainTrigger.register()
     renderTrigger.unregister()
+    finalTrigger.unregister()
     return
 }
 export default { toggle };

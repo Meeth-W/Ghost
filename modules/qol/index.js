4147855b1d294056ba70a37f37b02f134dcd858b
Meeth <meeth2111@gmail.com>
2024-12-17 18:52:02 +0530

    🚨 cleanup

---
+++ modules/qol/index.js
@@ -0,0 +1,17 @@
+import PogObject from "../../../PogData";
+import config from "../../config";
+
+
+const render = register('renderWorld', () => {
+
+})
+
+export function toggle() {
+    if (config().configToggle && config().toggle && config().toggleCheat) {
+
+        return
+    }
+
+    return
+}
+export default { toggle };
